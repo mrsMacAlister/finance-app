bada39829cd15e7bd42437f9d56e7483410f2221
mrsMacAlister <75312297+mrsMacAlister@users.noreply.github.com>
2023-02-12 23:33:01 +0100

    Calendar widget outline (important dates)

--- src/components/calendarW/CalendarW.jsx
+++ src/components/calendarW/CalendarW.jsx
@@ -1,7 +1,23 @@
 import "./calendarW.scss";
 
 const CalendarW = () => {
-  return <div className="calendarW">CalendarW</div>;
+  return (
+    <div className="calendarW">
+      <div className="top">
+        <div className="title">IMPORTANT DATES</div>
+      </div>
+      <div className="bottom">
+        <ul className="list">
+          <li className="item">03/03/2023 (Fri) car registration</li>
+          <li className="item">
+            01/04/2023 (Wed) purchase plane tickets to Maldives
+          </li>
+          <li className="item">05/06/2023 (Mon) Netflix membership (yearly)</li>
+          <li className="item">07/07/2023 (Fri) go to Maldives (2k)</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default CalendarW;
