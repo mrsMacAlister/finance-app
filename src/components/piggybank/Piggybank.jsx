f85579fe86e244a09c9312cc847950d8e3878294
mrsMacAlister <75312297+mrsMacAlister@users.noreply.github.com>
2023-03-04 23:02:21 +0100

    swiper/carousel

--- src/components/piggybank/Piggybank.jsx
+++ src/components/piggybank/Piggybank.jsx
@@ -1,23 +1,26 @@
 import "./piggybank.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+//import "swiper/css";
 
 const Piggybank = () => {
   return (
     <div className="piggybank">
       <div className="title">SAVINGS</div>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
-      </Swiper>
+      <div className="carousel">
+        <Swiper
+          className="swiper"
+          spaceBetween={10}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide className="swiper-slide">Slide 1 over here</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          ...
+        </Swiper>
+      </div>
       <img
         src="https://www.svgrepo.com/show/253917/piggy-bank-euro.svg"
         alt=""
